00ce2c9a6949b38ffc55a14894694c6573408f11
Mochammad Rezza Wikandito <reza.wikan.dito@gmail.com>
2024-08-07 00:07:10 +0800

    add migration

---
+++ database/migrations/2024_08_06_215411_create_saving_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saving_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('saving_id')->constrained('savings')->cascadeOnDelete();
+            $table->integer('amount');
+            $table->boolean('is_debit')->default(false);
+            $table->string('note')->nullable();
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saving_items');
+    }
+};
